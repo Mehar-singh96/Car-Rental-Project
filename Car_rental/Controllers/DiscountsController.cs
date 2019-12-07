3218e1fd27924cab8abf5155dfa02a1d26e7d632
Mehar Singh <mehar.singh@smu.ca>
2019-12-07 14:31:50 -0400

    Basic setup will be added

---
+++ Car_rental/Controllers/DiscountsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Car_rental.Models;
+
+namespace Car_rental.Controllers
+{
+    public class DiscountsController : Controller
+    {
+        private CarRentalModel db = new CarRentalModel();
+
+        // GET: Discounts
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Discounts.ToListAsync());
+        }
+
+        // GET: Discounts/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Discount discount = await db.Discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(discount);
+        }
+
+        // GET: Discounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Discounts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Points,DiscountPercent")] Discount discount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Discounts.Add(discount);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(discount);
+        }
+
+        // GET: Discounts/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Discount discount = await db.Discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(discount);
+        }
+
+        // POST: Discounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Points,DiscountPercent")] Discount discount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(discount).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(discount);
+        }
+
+        // GET: Discounts/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Discount discount = await db.Discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(discount);
+        }
+
+        // POST: Discounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Discount discount = await db.Discounts.FindAsync(id);
+            db.Discounts.Remove(discount);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Car_rental/Models/Discount.cs
@@ -0,0 +1,27 @@
+namespace Car_rental.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Discount")]
+    public partial class Discount
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Discount()
+        {
+            AccountDetails = new HashSet<AccountDetail>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Points { get; set; }
+
+        public int DiscountPercent { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<AccountDetail> AccountDetails { get; set; }
+    }
+}
